c3fd2fb0f490c091fc8c8adbf9b24559296426a1
RedLime <redlimerl@gmail.com>
2025-07-15 01:55:32 +0900

    try no filter old libraries?

--- src/goal/minecraft.ts
+++ src/goal/minecraft.ts
@@ -25,11 +25,11 @@ function transformVersion(version: PistonVersion): VersionOutput {
 	libraries = libraries.filter(x => !processLWJGL(x, requires, traits));
 
 	if (mainClass?.startsWith("net.minecraft.launchwrapper.")) {
-		libraries = libraries.filter(
-			x => !x.name.value.startsWith("net.minecraft:launchwrapper:")
-				&& x.name.group !== "net.sf.jopt-simple"
-				&& x.name.group !== "org.ow2.asm"
-		);
+		// libraries = libraries.filter(
+		// 	x => !x.name.value.startsWith("net.minecraft:launchwrapper:")
+		// 		&& x.name.group !== "net.sf.jopt-simple"
+		// 		&& x.name.group !== "org.ow2.asm"
+		// );
 
 		// mainClass = undefined;
 		traits.push(VersionFileTrait.LegacyLaunch);
