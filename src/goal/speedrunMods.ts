a11c143ff7c2343809cb452f427104eae8544e53
RedLime <redlimerl@gmail.com>
2025-08-25 22:18:20 +0900

    update separated mod list for mcsrranked

--- src/goal/speedrunMods.ts
+++ src/goal/speedrunMods.ts
@@ -1,5 +1,6 @@
 import { defineGoal, type VersionOutput } from "#core/goal.ts";
 import speedrunModVersions from "#provider/speedrunModVersions.ts";
+import type { SpeedrunModIndex } from "#schema/speedrun/modsIndex.ts";
 
 export default defineGoal({
     id: "org.mcsr.mods",
@@ -7,12 +8,24 @@ export default defineGoal({
     provider: speedrunModVersions,
 
     generate(info): VersionOutput[] {
-        const date = new Date();
-        return [{
-            version: `verified`,
-            releaseTime: date.toISOString(),
-            mods: info.mods
-        }];
+		const standardSettingsId = "standardsettings";
+		const defaultMCSRRankedMods = ["antigone", "fast_reset", "krypton", "lazydfu", "lazystronghold", "lithium", "sodium", "sodiummac", "sodiummac", "starlight", "voyager", "speedrunapi", "antiresourcereload", "state-output", "speedrunigt", "boundlesswindow", "retino"];
+
+        return [
+			getMods("verified", info.mods),
+			getMods("mcsrranked-all", info.mods),
+			getMods("mcsrranked-standardsettings", info.mods.filter(m => defaultMCSRRankedMods.includes(m.modid) || m.modid == standardSettingsId)),
+			getMods("mcsrranked-basic", info.mods.filter(m => defaultMCSRRankedMods.includes(m.modid))),
+		];
     },
     recommend: () => false,
-})
+});
+
+function getMods(name: string, list: SpeedrunModIndex[]): VersionOutput {
+	const date = new Date();
+	return {
+		version: name,
+		releaseTime: date.toISOString(),
+		mods: list
+	}
+}
